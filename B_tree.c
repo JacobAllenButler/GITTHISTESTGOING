f7d8aeb4e84ffab7be95551e9a0ef2a4b50f29e0
Jacob Butler <jacoballenbutler@gmail.com>
2020-03-05 17:25:31 -0500

    add c file

---
+++ B_tree.c
@@ -0,0 +1,36 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+struct node
+{
+  int data;
+  struct node *left,*right,*parent;
+};
+struct node* initNode(int data)
+{
+  struct node* node = (struct node*)malloc(20*data);
+  node->data = data;
+  node->left = NULL;
+  node->right = NULL;
+  node->parent = NULL;
+  return(node);
+}
+
+struct node* insert(int data, struct node* root)
+{
+
+}
+
+void printPreorder(struct node* node)
+{
+
+}
+
+int main()
+{
+  struct node *root = initNode(1);
+  root->left = initNode(2);
+  printf("here\n");
+  printf("%d\n",root->data);
+  //printf("%d\n",root->left->data);
+}
